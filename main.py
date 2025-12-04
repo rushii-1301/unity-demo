d41fe8f7c3de177b3c7f2b12532be3394ce66bbf
gautam1301 <gautamhadiya83@gmail.com>
2025-12-04 22:35:43 +0530

    sasa

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from routers.lectures import router as lecture_router
 
 app = FastAPI(title="Lecture API")
 
-# Mount static folder for JSON + audio
+# Mount static root (optional)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Register lecture routes
+# Mount chapter materials folder explicitly
+app.mount(
+    "/chapter-materials",
+    StaticFiles(directory="static/chapter-materials"),
+    name="chapter-materials"
+)
+
+# Mount lectures JSON folder
+app.mount(
+    "/lectures",
+    StaticFiles(directory="static/lectures"),
+    name="lectures"
+)
+
+# Include your router
 app.include_router(lecture_router)
